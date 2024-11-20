409aae77271633435e31ec78dc211513fc2eb240
Wellington <w9llington@gmail.com>
2024-11-20 18:57:46 -0300

    API integration

--- src/components/ProductDetails/index.tsx
+++ src/components/ProductDetails/index.tsx
@@ -1,13 +1,49 @@
-// ProductDetails.tsx
+
 import { Link, useParams } from 'react-router-dom';
-import { MenuList } from '../Menu';
+// import { MenuList } from '../Menu';
 import {  Background, HomeContainer, Lista, MainHome } from './style';
 import logo from '../../assets/logo.png';
 import Cart from '../Cart';
+import { useEffect, useState } from 'react';
+
+export interface Cardapio  {
+  foto:string 
+  preco:number
+  id:number
+  nome:string
+  descricao:string
+  porcao:string
+}
+
+export type Restaurant = {
+  id: number
+  titulo: string
+  destacado: boolean
+  tipo: string
+  avaliacao: number
+  descricao: string
+  capa:string
+  cardapio: Cardapio[]
+}
+
+const getDescription = (description:string) => {
+  if(description.length > 95){
+    return description.slice(0,92) + '...'
+  }
+  return description
+}
 
 const ProductDetails = ({ cartItems, addToCart, clearCart, isCartOpen, setIsCartOpen }: any) => {
-  const { id } = useParams<{ id: string }>();
-  const menuItem = MenuList.find((item:any) => item.id === parseInt(id || '', 10));
+  const {id} = useParams()
+
+  const [menuItem, setmenuItem] = useState<Restaurant>()
+
+  useEffect(() => { 
+    fetch(`https://fake-api-tau.vercel.app/api/efood/restaurantes/${id}`)
+    .then(res => res.json())
+    .then(res => setmenuItem(res))
+  },[id])
+  
 
   if (!menuItem) {
     return <p>Item não encontrado</p>;
@@ -29,19 +65,19 @@ const ProductDetails = ({ cartItems, addToCart, clearCart, isCartOpen, setIsCart
         </div>
       </HomeContainer>
       
-      <Background img={menuItem.url}  >
+      <Background img={menuItem.capa}  >
         <div className="wrapper">
-          <span>{menuItem.origin}</span>
-          <h2>{menuItem.name}</h2>
+          <span>{menuItem.tipo}</span>
+          <h2>{menuItem.titulo}</h2>
         </div>        
       </Background>
      
       <Lista>
-        {menuItem.menu.map((menuItem:any) => (
+        {menuItem.cardapio.map((menuItem:any) => (
           <li key={menuItem.name}>
-            <img src={menuItem.img} alt={menuItem.name} />
-            <h3>{menuItem.name}</h3>
-            <p>{menuItem.description}</p>
+            <img src={menuItem.foto} alt={menuItem.nome} />
+            <h3>{menuItem.nome}</h3>
+            <p>{getDescription(menuItem.descricao)}</p>
             <button type="button" onClick={() => addToCart(menuItem)}>
               Adicionar ao Carrinho
             </button>
